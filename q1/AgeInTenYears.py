061fc54e256a388ea4a197d5adf9c900b8c389a4
Ada <ampcbayola@brc.pshs.edu.ph>
2026-08-26 09:14:22 +0800

    Add AgeInTenYears.py to calculate future age

---
+++ q1/AgeInTenYears.py
@@ -0,0 +1,11 @@
+name = str(input("Your name: "))
+section = str(input("Your section: "))
+BirthYear = int(input("Your birth year: "))
+
+
+AgeIn10Years = 2036 - BirthYear
+
+
+print("Your name is, ", name)
+print("Your section is, ", section)
+print("your age 10 years from now, ", AgeIn10Years)
