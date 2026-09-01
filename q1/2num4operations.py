d7886937f5b86f126bd1991eb8d5ebcf10d3f9f0
Ada <ampcbayola@brc.pshs.edu.ph>
2026-09-01 19:28:16 +0800

    Add basic arithmetic operations for two numbers

---
+++ q1/2num4operations.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+
+print(f"\n The sum: {sum} \n The difference: {diff} \n The product: {product} \n The quotient: {quotient}")
